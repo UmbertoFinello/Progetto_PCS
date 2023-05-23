3de36e945039ea20cde43b7fa174ed6b2d6c6637
fabiodanielediena <126665927+fabiodanielediena@users.noreply.github.com>
2023-05-23 12:28:56 +0200

    Update Delaunay_class.hpp

--- Projects/Delaunay/src/Delaunay_class.hpp
+++ Projects/Delaunay/src/Delaunay_class.hpp
@@ -1,15 +1,1 @@
-#ifndef __DELAUNAY_H
-#define __DELAUNAY_H
 
-#include <iostream>
-
-namespace ProjectLibrary
-{
-  class Empty
-  {
-    public:
-      void Show() const { std::cout<< "Hello world;"<< std::endl; }
-  };
-}
-
-#endif // __DELAUNAY_H
