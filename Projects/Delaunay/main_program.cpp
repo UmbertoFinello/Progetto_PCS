76c3b3f1a79e241d50481b3d420622f9eced231d
Fabio Daniele Diena <s282265@studenti.polito.it>
2023-05-23 12:06:41 +0200

    main program

--- Projects/Delaunay/main_program.cpp
+++ Projects/Delaunay/main_program.cpp
@@ -2,9 +2,15 @@
 
 int main()
 {
-  ProjectLibrary::Empty empty;
-
-  empty.Show();
-
-  return 0;
+    ProjectLibrary::IOmesh IO = IOmesh();
+    Vector<Punti> List_punti;
+    if(!(IO.ImportMesh(List_punti,'./Dataset/Test1.csv')))
+        return -1;
+    else{
+        ProjectLibrary::Mesh mesh = Mesh(List_punti);
+        mesh.Show();
+        if(!(IO.ExportMesh(mesh, './Result/')))
+            return -2;
+    }
+    return 0;
 }
