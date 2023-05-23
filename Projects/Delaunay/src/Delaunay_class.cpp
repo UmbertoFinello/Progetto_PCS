d66518e2cfb51539d903f51dd3dc5e1e1d52de6e
Umberto <s283219@studenti.polito.it>
2023-05-23 14:38:29 +0200

    update Umbi

--- Projects/Delaunay/src/Delaunay_class.cpp
+++ Projects/Delaunay/src/Delaunay_class.cpp
@@ -2,4 +2,48 @@
 
 namespace ProjectLibrary
 {
-}
+    Triangolo::Triangolo(vector<Punto>& v, unsigned int identificatore, const vector<Lato>& l):
+        _vertici(v), _id(identificatore), _lati(l)
+    {
+        this->OrdinamentoAntiorario();
+    }
+
+    Triangolo::OrdinamentoAntiorario()
+    {
+        double Area = (0.5)*(_vertici[0]_x*_vertici[1]_y + _vertici[0]_y*_vertici[2]_x + _vertici[1]_x*_vertici[2]_y - _vertici[2]_x*_vertici[1]_y - _vertici[2]_y*vertici[0]_x - _vertici[1]_x*_vertici[0]_y);
+        if (Area < 0)       //TOLLERANZA
+        {
+            Punto a = _vertici[1];
+            _vertici[1] = Punto(_vertici[2]);
+            _vertici[2] = a;
+        }
+        return 0;
+    }
+
+    Triangolo::CalcolaAngolo(const Lato& segm)
+    {
+        unsigned int l1 = 3;
+        unsigned int l2 = 3;
+        for (unsigned int i = 0; i < 3; i++)
+        {
+            if ( _lati[i]._id != segm._id)
+            {
+               if(  l1 == 3)
+               {
+                   l1 = i;
+               }
+               else if ( l2 == 3)
+               {
+                   l2 = i;
+               }
+            }
+        }
+
+
+        double cosenoAngolo = ((l[0]._length)^2 + (l[1]._length)^2 - (segm._length * segm._length) / (2 * l[0]._length * l[1]._length);
+        double angoloRad = acos(cosenoAngolo);
+        // double angoloDeg = angoloRad * 180.0 / M_PI;
+        return angoloRad;
+    }
+
+};
--- Projects/Delaunay/src/Delaunay_class.hpp
+++ Projects/Delaunay/src/Delaunay_class.hpp
@@ -2,14 +2,76 @@
 #define __DELAUNAY_H
 
 #include <iostream>
+#include "Eigen/Eigen"
+//#include <algorithm>
+#include <cmath>
 
+using namespace std;
+using namespace Eigen;
 namespace ProjectLibrary
 {
-  class Empty
-  {
+
+    class Punto
+    {
+    friend class Lato;
+    friend class Triangolo;
+    protected:
+        unsigned int _id;
+        double _x;
+        double _y;
     public:
-      void Show() const { std::cout<< "Hello world;"<< std::endl; }
-  };
+        Punto(unsigned int& id , double& x, double& y);
+        Punto(const Punto& p);
+    };
+
+
+    class Lato
+    {
+    friend class Punto;
+    friend class Triangolo;
+    protected:
+        unsigned int _id;
+        Punto _p1;
+        Punto _p2;
+        double _length;
+    public:
+        Lato(unsigned int& id , Punto& p1, Punto& p2);
+
+    };
+
+    class Triangolo
+    {
+    friend class Punto;
+    friend class Lato;
+    protected:
+        vector<Punto> _vertici;
+        int _id;
+        vector<Lato> _lati;
+    public:
+        Triangolo(vector<Punto>& v, unsigned int identificatore, const vector<Lato>& l);
+        double CalcolaAngolo(const Lato& segm);
+        void OrdinamentoAntiorario();
+    };
+
+    class Mesh
+    {
+    protected:
+        vector<Triangolo> _listaTriangoli;
+        vector<Lato> _listaLati;
+        vector<Punto> _listaPunti;
+        //MatrixXd<unsigned int> _adiacenza;
+    public:
+        Mesh(const vector<Punto>& listaPunti , string OutFilePath);
+    };
+
+
+    class IOMesh
+    {
+    public:
+        vector<Punto> ImportMesh(const string& FileName);
+        void ExportMesh(const Mesh& mesh);
+    };
+
 }
 
 #endif // __DELAUNAY_H
