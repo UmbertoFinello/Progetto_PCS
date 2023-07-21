ff6775bbc4ba1b1cf01c006ab9ce1da1b54a2270
fabio diena <s282265@studenti.polito.it>
2023-07-21 19:11:15 +0200

    tempo esecuzione

--- Projects/Delaunay/main_program.cpp
+++ Projects/Delaunay/main_program.cpp
@@ -1,18 +1,27 @@
 #include "Delaunay_class.hpp"
+#include <chrono>
 
 using namespace ProjectLibrary;
 
 int main()
 {
+    auto start = chrono::steady_clock::now();
+
     vector<Punto> List_punti;
-    string inputfile = "../Delaunay/Dataset/Test2.csv";
+    string inputfile = "../Delaunay/Dataset/Test1.csv";
     if(!(ImportPunti(List_punti, inputfile)))
         return -1;
     else{
         Mesh griglia = Mesh(List_punti);
         cout << griglia.Show();
-        if(!(griglia.ExportMesh("../Delaunay/Result/", "Test2.csv")))
+        if(!(griglia.ExportMesh("../Delaunay/Result/", "Test1.csv")))
             return -2;
     }
+    auto end = chrono::steady_clock::now();
+
+    cout << "Elapsed time in microseconds: "
+         << chrono::duration_cast<chrono::microseconds>(end - start).count()
+         << endl;
+
     return 0;
 }
